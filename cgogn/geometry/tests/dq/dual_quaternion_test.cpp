f8ca1383e76e33001944836747c1565a36b46bd5
Thomas Torterotot <thomas.torterotot@etu.unistra.fr>
2024-01-31 17:45:58 +0100

    comment DualQuaternion tests
    Also fixes a double semicolon typo

--- cgogn/geometry/tests/dq/dual_quaternion_test.cpp
+++ cgogn/geometry/tests/dq/dual_quaternion_test.cpp
@@ -37,12 +37,15 @@ public:
 	constexpr static geometry::Scalar prec = Eigen::NumTraits<geometry::Scalar>::dummy_precision();
 };
 
+// Test if DualQuaternion::identity is indeed the identity DQ
 TEST_F(DualQuaternionTest, identity)
 {
 	EXPECT_TRUE((DualQuaternion::identity().rotation().isApprox(Quaternion{1, 0, 0, 0})));
 	EXPECT_TRUE((DualQuaternion::identity().translation().squaredNorm() <= prec * prec));
 }
 
+// Test if DualQuaternion::isApprox is complete and correct, especially in regards to precision
+// Attention is brought to false positives because we use this method to check results in later tests
 TEST_F(DualQuaternionTest, isApprox)
 {
 	DualQuaternion a = DualQuaternion::from_rt({0.0625, 1.0, 16.0, 1024.0}, {256.0, 0.0, -256.0});
@@ -54,81 +57,92 @@ TEST_F(DualQuaternionTest, isApprox)
 	DualQuaternion y = DualQuaternion::from_translation({4.0 * prec, 0.0, 0.0});
 	DualQuaternion z = DualQuaternion::from_translation({0.25 * prec, 0.0, 0.0});
 
-	EXPECT_FALSE(a.isApprox(DualQuaternion::identity()));
-	EXPECT_TRUE(a.isApprox(b));
-	EXPECT_TRUE(a.isApprox(c, prec));
-	EXPECT_FALSE(a.isApprox(d, prec));
-	EXPECT_FALSE(y.isApprox(DualQuaternion::identity(), prec));
-	EXPECT_TRUE(z.isApprox(DualQuaternion::identity(), prec));
+	EXPECT_FALSE(a.isApprox(DualQuaternion::identity())); // clearly different values
+	EXPECT_TRUE(a.isApprox(b)); // same value
+	EXPECT_TRUE(a.isApprox(c, prec)); // close enough value
+	EXPECT_FALSE(a.isApprox(d, prec)); // too far apart
+	EXPECT_FALSE(y.isApprox(DualQuaternion::identity(), prec)); // too far apart
+	EXPECT_TRUE(z.isApprox(DualQuaternion::identity(), prec)); // close enough value
 }
 
+// Test if DualQuaternion::from_rotation initializes the DQ properly
 TEST_F(DualQuaternionTest, from_rotation)
 {
 	Quaternion r{1.0, 2.0, 4.0, 8.0};
 	DualQuaternion q = DualQuaternion::from_rotation(r);
-	EXPECT_TRUE(q.rotation().isApprox(r.normalized()));
-	EXPECT_TRUE(q.translation().squaredNorm() <= prec * prec);
+	EXPECT_TRUE(q.rotation().isApprox(r.normalized())); // (normalized) rotation preserved
+	EXPECT_TRUE(q.translation().squaredNorm() <= prec * prec); // no translation
 }
 
+// Test if DualQuaternion::from_translation initializes the DQ properly
 TEST_F(DualQuaternionTest, from_translation)
 {
 	Vec3 t{1.0, 2.0, 4.0};
 	DualQuaternion q = DualQuaternion::from_translation(t);
-	EXPECT_TRUE(q.rotation().isApprox(Quaternion{1, 0, 0, 0}));
-	EXPECT_TRUE(q.translation().isApprox(t));
+	EXPECT_TRUE(q.rotation().isApprox(Quaternion{1, 0, 0, 0})); // no rotation
+	EXPECT_TRUE(q.translation().isApprox(t)); // translation preserved
 }
 
+// Test if DualQuaternion::from_point initializes the DQ properly
 TEST_F(DualQuaternionTest, from_point)
 {
 	Vec3 p{1.0, 2.0, 4.0};
-	EXPECT_TRUE(DualQuaternion::from_point(p).point().isApprox(p));
+	EXPECT_TRUE(DualQuaternion::from_point(p).point().isApprox(p)); // point preserved
 }
 
+// Test if DualQuaternion::from_rt initializes the DQ properly
 TEST_F(DualQuaternionTest, from_rt)
 {
+	// r is already normalized but we pretend it isn't and still normalize it
 	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 2.0, 4.0}, Vec3{-1.0, -0.5, -0.25});
 	Vec3 t{1.0, 2.0, 4.0};
 	DualQuaternion q = DualQuaternion::from_rt(r, t);
-	EXPECT_TRUE(q.rotation().isApprox(r.normalized()));
-	EXPECT_TRUE(q.translation().isApprox(r.normalized()._transformVector(t)));
+	EXPECT_TRUE(q.rotation().isApprox(r.normalized())); // (normalized) rotation preserved
+	EXPECT_TRUE(q.translation().isApprox(r.normalized()._transformVector(t))); // translation rotated
 }
 
+// Test if DualQuaternion::from_tr initializes the DQ properly
 TEST_F(DualQuaternionTest, from_tr)
 {
+	// r is already normalized but we pretend it isn't and still normalize it
 	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 2.0, 4.0}, Vec3{-1.0, -0.5, -0.25});
 	Vec3 t{1.0, 2.0, 4.0};
 	DualQuaternion q = DualQuaternion::from_tr(t, r);
-	EXPECT_TRUE(q.rotation().isApprox(r.normalized()));
-	EXPECT_TRUE(q.translation().isApprox(t));
+	EXPECT_TRUE(q.rotation().isApprox(r.normalized())); // (normalized) rotation preserved
+	EXPECT_TRUE(q.translation().isApprox(t)); // translation preserved
 }
 
+// Test if DualQuaternion::identity is indeed identity (with DQ created with from_rt)
 TEST_F(DualQuaternionTest, identity_rt)
 {
 	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 2.0, 4.0}, Vec3{-1.0, -0.5, -0.25});
 	Vec3 t{1.0, 2.0, 4.0};
 	DualQuaternion q = DualQuaternion::from_rt(r, t);
-	EXPECT_TRUE(q.isApprox(q * DualQuaternion::identity()));
-	EXPECT_TRUE(q.isApprox(DualQuaternion::identity() * q));
+	EXPECT_TRUE(q.isApprox(q * DualQuaternion::identity())); // as right operand
+	EXPECT_TRUE(q.isApprox(DualQuaternion::identity() * q)); // as left operand
 }
 
+// Test if DualQuaternion::identity is indeed identity (with DQ created with from_tr)
 TEST_F(DualQuaternionTest, identity_tr)
 {
 	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 2.0, 4.0}, Vec3{-1.0, -0.5, -0.25});
 	Vec3 t{1.0, 2.0, 4.0};
 	DualQuaternion q = DualQuaternion::from_tr(t, r);
-	EXPECT_TRUE(q.isApprox(q * DualQuaternion::identity()));
-	EXPECT_TRUE(q.isApprox(DualQuaternion::identity() * q));
+	EXPECT_TRUE(q.isApprox(q * DualQuaternion::identity())); // as right operand
+	EXPECT_TRUE(q.isApprox(DualQuaternion::identity() * q)); // as left operand
 }
 
+// Test if DualQuaternion::normalized can normalize a non-normalized DQ
 TEST_F(DualQuaternionTest, normalized)
 {
 	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 2.0, 4.0}, Vec3{-1.0, -0.5, -0.25});
 	Vec3 t{1.0, 2.0, 4.0};
 	DualQuaternion q = DualQuaternion::from_rt(r, t) * 2.0;
-	EXPECT_FALSE(almost_equal_relative(q.magnitude(), geometry::Scalar(1)));
-	EXPECT_TRUE(almost_equal_relative(q.normalized().magnitude(), geometry::Scalar(1)));;
+	EXPECT_FALSE(almost_equal_relative(q.magnitude(), geometry::Scalar(1))); // scaled not normalized
+	EXPECT_TRUE(almost_equal_relative(q.normalized().magnitude(), geometry::Scalar(1)));
 }
 
+// Test if DualQuaternion::normalize is indeed the in-place equivalent of DualQuaternion::normalized
 TEST_F(DualQuaternionTest, normalize_normalized)
 {
 	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 2.0, 4.0}, Vec3{-1.0, -0.5, -0.25});
@@ -139,16 +153,21 @@ TEST_F(DualQuaternionTest, normalize_normalized)
 	EXPECT_TRUE(q.normalized().isApprox(q_));
 }
 
+// Test if DualQuaternion::conjugated is indeed the conjugate: q q* == q* q != identity
+// We check it's not the identity because we don't expect it to be the case for this value of q
+// and it would imply q q* == q* q by itself (informally, if q q* != q* q == identity,
+// what would even be the value of q q* given it intuitively remains a sort of "mirror" of q* q?)
 TEST_F(DualQuaternionTest, conjugated)
 {
 	Quaternion r{1.0, 2.0, 4.0, 8.0};
 	Vec3 t{1.0, 2.0, 4.0};
 	DualQuaternion q = DualQuaternion::from_rt(r, t);
-	EXPECT_TRUE((q * q.conjugated()).isApprox(q.conjugated() * q));
-	EXPECT_FALSE(DualQuaternion::identity().isApprox(q * q.conjugated()));
-	EXPECT_FALSE(DualQuaternion::identity().isApprox(q.conjugated() * q));
+	EXPECT_TRUE((q * q.conjugated()).isApprox(q.conjugated() * q)); // q q* == q* q
+	EXPECT_FALSE(DualQuaternion::identity().isApprox(q * q.conjugated())); // q q* != identity
+	EXPECT_FALSE(DualQuaternion::identity().isApprox(q.conjugated() * q)); // q* q != identity
 }
 
+// Test if DualQuaternion::conjugate is indeed the in-place equivalent of DualQuaternion::conjugated
 TEST_F(DualQuaternionTest, conjugate_conjugated)
 {
 	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 2.0, 4.0}, Vec3{-1.0, -0.5, -0.25});
@@ -159,18 +178,22 @@ TEST_F(DualQuaternionTest, conjugate_conjugated)
 	EXPECT_TRUE(q.conjugated().isApprox(q_));
 }
 
+// Test if DualQuaternion::identity's transform is indeed a no-op
 TEST_F(DualQuaternionTest, transform_identity)
 {
 	Vec3 p{1.0, 2.0, 4.0};
 	EXPECT_TRUE(DualQuaternion::identity().transform(p).isApprox(p));
 }
 
+// Test if DualQuaternion::transform produces the expected results
 TEST_F(DualQuaternionTest, transform_rt_tr)
 {
-	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 0.0, 0.0}, Vec3{0.0, 1.0, 0.0});
-	Vec3 t{0.0, 1.0, 1.0};
-	Vec3 p{-1.0, -1.0, 0.0};
+	Quaternion r = Quaternion::FromTwoVectors(Vec3{1.0, 0.0, 0.0}, Vec3{0.0, 1.0, 0.0}); // pi/2 around Z
+	Vec3 t{0.0, 1.0, 1.0}; // translation
+	Vec3 p{-1.0, -1.0, 0.0}; // initial point
+	// (-1, -1, 0) --t-> (-1, 0, 1) --r-> (0, -1, 1)
 	EXPECT_TRUE(DualQuaternion::from_rt(r, t).transform(p).isApprox(Vec3{0.0, -1.0, 1.0}));
+	// (-1, -1, 0) --r-> (1, -1, 0) --t-> (1, 0, 1)
 	EXPECT_TRUE(DualQuaternion::from_tr(t, r).transform(p).isApprox(Vec3{1.0, 0.0, 1.0}));
 }
 

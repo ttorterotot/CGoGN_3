56da299482c5ac961f1d7faacad942d2f0bbb761
Thomas Torterotot <thomas.torterotot@etu.unistra.fr>
2024-02-13 12:38:36 +0100

    convert indentation to tabs in keyframed_animation.h

--- cgogn/geometry/types/animation/keyframed_animation.h
+++ cgogn/geometry/types/animation/keyframed_animation.h
@@ -37,108 +37,108 @@ namespace geometry
 template <typename TimeT, typename TransformT>
 struct AnimationKeyframe
 {
-    using Time = TimeT;
-    using Transform = TransformT;
+	using Time = TimeT;
+	using Transform = TransformT;
 
-    TimeT time_;
-    TransformT transform_;
+	TimeT time_;
+	TransformT transform_;
 
-    AnimationKeyframe(TimeT time, TransformT transform)
-        : time_(time), transform_(transform) {}
+	AnimationKeyframe(TimeT time, TransformT transform)
+		: time_(time), transform_(transform) {}
 };
 
 template <template <typename> typename ContainerT, typename TimeT, typename TransformT>
 class KeyframedAnimation : public ContainerT<AnimationKeyframe<TimeT, TransformT>>
 {
 public:
-    using Keyframe = AnimationKeyframe<TimeT, TransformT>;
-    static_assert(!std::is_integral_v<TimeT>, "Time type should not be integral");
+	using Keyframe = AnimationKeyframe<TimeT, TransformT>;
+	static_assert(!std::is_integral_v<TimeT>, "Time type should not be integral");
 
 private:
 
-    static constexpr const auto CompareKeyframes = [](const Keyframe& a, const Keyframe& b)
-    {
-        return a.time_ < b.time_;
-    };
+	static constexpr const auto CompareKeyframes = [](const Keyframe& a, const Keyframe& b)
+	{
+		return a.time_ < b.time_;
+	};
 
-    template <typename T, typename S>
-    static inline T default_lerp(const T& a, const T& b, const S& t)
-    {
-        return (1.0 - t) * a + t * b;
-    }
+	template <typename T, typename S>
+	static inline T default_lerp(const T& a, const T& b, const S& t)
+	{
+		return (1.0 - t) * a + t * b;
+	}
 
 public:
 
-    using ContainerT<Keyframe>::ContainerT; // inherit container's constructors
+	using ContainerT<Keyframe>::ContainerT; // inherit container's constructors
 
-    /// @brief Evaluates if the animation's keyframes are sorted in the container.
-    /// @return whether or not the keyframes are sorted
-    [[nodiscard]]
-    inline bool is_sorted() const
-    {
-        return std::is_sorted(ContainerT<Keyframe>::cbegin(), ContainerT<Keyframe>::cend(), CompareKeyframes);
-    }
+	/// @brief Evaluates if the animation's keyframes are sorted in the container.
+	/// @return whether or not the keyframes are sorted
+	[[nodiscard]]
+	inline bool is_sorted() const
+	{
+		return std::is_sorted(ContainerT<Keyframe>::cbegin(), ContainerT<Keyframe>::cend(), CompareKeyframes);
+	}
 
-    /// @brief Sorts keyframes by time, useful if it's unknown whether they were added in order or not.
-    /// @param stable whether or not to use a stable sort (preserves the order of equal elements)
-    inline void sort(bool stable = true)
-    {
-        if (stable)
-            std::stable_sort(ContainerT<Keyframe>::begin(), ContainerT<Keyframe>::end(), CompareKeyframes);
-        else
-            std::sort(ContainerT<Keyframe>::begin(), ContainerT<Keyframe>::end(), CompareKeyframes);
-    }
+	/// @brief Sorts keyframes by time, useful if it's unknown whether they were added in order or not.
+	/// @param stable whether or not to use a stable sort (preserves the order of equal elements)
+	inline void sort(bool stable = true)
+	{
+		if (stable)
+			std::stable_sort(ContainerT<Keyframe>::begin(), ContainerT<Keyframe>::end(), CompareKeyframes);
+		else
+			std::sort(ContainerT<Keyframe>::begin(), ContainerT<Keyframe>::end(), CompareKeyframes);
+	}
 
-    /// @brief Finds the earliest and latest keyframes.
-    /// If the keyframes can be assumed to be sorted, use `operator[]` on `0` and `size() - 1` instead.
-    /// @return a pair containing the most extreme keyframes
-    template <typename It>
-    [[nodiscard]]
-    inline std::pair<It, It> minmax_element() const
-    {
-        return std::minmax_element(ContainerT<Keyframe>::cbegin(), ContainerT<Keyframe>::cend(), CompareKeyframes);
-    }
+	/// @brief Finds the earliest and latest keyframes.
+	/// If the keyframes can be assumed to be sorted, use `operator[]` on `0` and `size() - 1` instead.
+	/// @return a pair containing the most extreme keyframes
+	template <typename It>
+	[[nodiscard]]
+	inline std::pair<It, It> minmax_element() const
+	{
+		return std::minmax_element(ContainerT<Keyframe>::cbegin(), ContainerT<Keyframe>::cend(), CompareKeyframes);
+	}
 
-    /// @brief Interpolates a transform between both neighboring keyframes.
-    /// Requires keyframes to be sorted, see sort().
-    /// Converts transforms to interpolate - using `to_interpolation_space` -
-    /// then either returns the result - constant extrapolation - outside keyframes,
-    /// or interpolates between both neighboring transforms - using `interpolate` - between keyframes.
-    /// The possible conversion of the return value from interpolation space is left to the caller.
-    /// @param time the time value to interpolate for
-    /// @param to_interpolation_space a mapping function from storage to interpolation space in the form
-    ///                               `(const TransformT&) -> InterpolatedT`
-    /// @param interpolate an interpolation function in the form
-    ///                    `(const InterpolatedT&, const InterpolatedT&, const TimeT&) -> InterpolatedT`
-    /// @return the interpolated transform as `InterpolatedT`
-    template <typename InterpolatedT = TransformT,
-            typename T = decltype(identity_c<const InterpolatedT&>),
-            typename U = decltype(default_lerp<InterpolatedT, TimeT>)>
-    [[nodiscard]]
-    inline InterpolatedT get_transform(TimeT time,
-            T to_interpolation_space = identity_c<InterpolatedT>,
-            U interpolate = default_lerp<InterpolatedT, TimeT>) const
-    {
-        // Not using ::empty so ContainerT doesn't have to implement it
-        cgogn_assert(ContainerT<Keyframe>::size() > 0);
+	/// @brief Interpolates a transform between both neighboring keyframes.
+	/// Requires keyframes to be sorted, see sort().
+	/// Converts transforms to interpolate - using `to_interpolation_space` -
+	/// then either returns the result - constant extrapolation - outside keyframes,
+	/// or interpolates between both neighboring transforms - using `interpolate` - between keyframes.
+	/// The possible conversion of the return value from interpolation space is left to the caller.
+	/// @param time the time value to interpolate for
+	/// @param to_interpolation_space a mapping function from storage to interpolation space in the form
+	///                               `(const TransformT&) -> InterpolatedT`
+	/// @param interpolate an interpolation function in the form
+	///                    `(const InterpolatedT&, const InterpolatedT&, const TimeT&) -> InterpolatedT`
+	/// @return the interpolated transform as `InterpolatedT`
+	template <typename InterpolatedT = TransformT,
+			typename T = decltype(identity_c<const InterpolatedT&>),
+			typename U = decltype(default_lerp<InterpolatedT, TimeT>)>
+	[[nodiscard]]
+	inline InterpolatedT get_transform(TimeT time,
+			T to_interpolation_space = identity_c<InterpolatedT>,
+			U interpolate = default_lerp<InterpolatedT, TimeT>) const
+	{
+		// Not using ::empty so ContainerT doesn't have to implement it
+		cgogn_assert(ContainerT<Keyframe>::size() > 0);
 
-        auto it = std::find_if(ContainerT<Keyframe>::cbegin(), ContainerT<Keyframe>::cend(),
-                [&time](const Keyframe& k){ return k.time_ > time; });
+		auto it = std::find_if(ContainerT<Keyframe>::cbegin(), ContainerT<Keyframe>::cend(),
+				[&time](const Keyframe& k){ return k.time_ > time; });
 
-        if (it == ContainerT<Keyframe>::cbegin())
-            return to_interpolation_space((*this)[0].transform_);
+		if (it == ContainerT<Keyframe>::cbegin())
+			return to_interpolation_space((*this)[0].transform_);
 
-        if (it == ContainerT<Keyframe>::cend())
-            return to_interpolation_space((*this)[ContainerT<Keyframe>::size() - 1].transform_);
+		if (it == ContainerT<Keyframe>::cend())
+			return to_interpolation_space((*this)[ContainerT<Keyframe>::size() - 1].transform_);
 
-        const Keyframe& before = *(it - 1);
-        const Keyframe& after = *it;
+		const Keyframe& before = *(it - 1);
+		const Keyframe& after = *it;
 
-        return interpolate(
-                to_interpolation_space(before.transform_),
-                to_interpolation_space(after.transform_),
-                (time - before.time_) / (after.time_ - before.time_));
-    }
+		return interpolate(
+				to_interpolation_space(before.transform_),
+				to_interpolation_space(after.transform_),
+				(time - before.time_) / (after.time_ - before.time_));
+	}
 };
 
 } // namespace geometry

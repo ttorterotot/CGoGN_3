baae103be3f0cc22ca3530566df979809a304b0e
Thomas Torterotot <thomas.torterotot@etu.unistra.fr>
2024-03-11 22:24:51 +0100

    implement skeleton and animation import from FBX data

--- cgogn/io/fbx/fbx_importer.h
+++ cgogn/io/fbx/fbx_importer.h
@@ -33,6 +33,8 @@
 
 #include <cgogn/core/types/animation/animation_skeleton.h>
 #include <cgogn/geometry/types/animation/keyframed_animation.h>
+#include <cgogn/geometry/types/rigid_transformation.h>
+#include <cgogn/geometry/types/dual_quaternion.h>
 #include <cgogn/geometry/functions/bounding_box.h>
 #include <cgogn/io/surface/surface_import.h>
 
@@ -49,6 +51,18 @@ namespace io
 
 class FbxImporterBase
 {
+public:
+	enum RotationOrder
+	{
+		Base = 8, // using octal for a clearer representation than binary while keeping size down
+		XYZ = 0012, // Z -> Y -> X
+		XZY = 0021, // Y -> Z -> X
+		YXZ = 0102, // Z -> X -> Y
+		YZX = 0120, // X -> Z -> Y
+		ZXY = 0201, // Y -> X -> Z
+		ZYX = 0210, // X -> Y -> Z
+	};
+
 protected:
 	using Skeleton = AnimationSkeleton;
 	using ObjectId = uint32;
@@ -91,6 +105,8 @@ protected:
 		ModelType type;
 		std::string name;
 		Properties properties;
+		Skeleton::Bone bone;
+		std::array<AnimationT*, 6> animation;
 	};
 
 	struct Geometry
@@ -126,6 +142,35 @@ protected:
 	/// @brief Opens and parses an FBX file
 	/// @param path the file path to open
 	void read(const std::string& path);
+
+	ObjectId get_parent_id(const ObjectId& child_id);
+
+	/// @brief Sets missing values for a property in `p` from `other_values`
+	/// @param p the property to fill (existing values take priority over those from `other_values`)
+	/// @param other_values the values to use
+	/// @param property_name the name of the property
+	/// @param warn_unequal whether or not to print a warning to stdout if the values are both present and different
+	template <typename OtherContainer>
+	void set_missing_values(Properties& p, const OtherContainer& other_values,
+			const std::string& property_name, bool warn_unequal)
+	{
+		auto pi = PROPERTY_INFO_.find(property_name);
+		if (pi == PROPERTY_INFO_.cend())
+			return;
+
+		const auto& [ptr, size] = pi->second(p);
+
+		cgogn_assert(other_values.size() == size);
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			if (!ptr[i].has_value())
+				ptr[i] = other_values[i];
+			else if (warn_unequal && other_values[i].has_value() && other_values[i].value() != ptr[i].value())
+				std::cout << "Warning: inconsistent values " << ptr[i].value() << " and " << other_values[i].value()
+						<< " for property " << property_name;
+		}
+	}
 
 private:
 	void read_root(std::istream& is);
@@ -189,6 +234,26 @@ private:
 	using FbxImporterBase::FbxImporterBase; // inherit constructor
 
 private:
+	template <typename ContainerOfOptionals>
+	geometry::Quaternion from_euler(const ContainerOfOptionals& xyz,
+			const RotationOrder& rotation_order = RotationOrder::ZYX)
+	{
+		auto res = geometry::Quaternion::Identity();
+		size_t ro = rotation_order;
+
+		for (int i = 0; i < 3; ++i)
+		{
+			size_t axis = ro % RotationOrder::Base;
+			ro /= RotationOrder::Base;
+
+			if (xyz[axis])
+				res = Eigen::AngleAxis<geometry::Quaternion::Scalar>(M_PI / 180.0 * xyz[axis].value(), AXES[axis])
+						* res;
+		}
+
+		return res;
+	}
+
 	void load_surfaces(Map<std::string, Surface*>& surfaces, bool normalized)
 	{
 		using Vertex = typename mesh_traits<Surface>::Vertex;
@@ -237,7 +302,198 @@ private:
 
 	void load_skeletons(Map<std::string, Skeleton*>& skeletons)
 	{
-		// TODO
+		if (std::find_if(models_.cbegin(), models_.cend(),
+				[](const Model& m) { return m.type == ModelType::LimbNode; }) == models_.cend())
+			return;
+
+		Skeleton* skeleton = new Skeleton{};
+
+		for (Model& m : models_)
+		{
+			if (m.type != ModelType::LimbNode)
+				continue;
+
+			m.bone = add_root(*skeleton);
+		}
+
+		for (Model& m : models_)
+		{
+			if (m.type != ModelType::LimbNode)
+				continue;
+
+			const ObjectId& parent_id = get_parent_id(m.id);
+
+			if (parent_id == INVALID_INDEX)
+				continue;
+
+			const auto parent_model_it = std::find_if(models_.begin(), models_.end(),
+					[&](const Model& parent) { return parent.id == parent_id; });
+
+			if (parent_model_it != models_.end())
+			{
+				cgogn_assert(parent_model_it->type == ModelType::LimbNode);
+				attach_bone(*skeleton, m.bone, parent_model_it->bone);
+			}
+		}
+
+		// Handle bone animations
+		// TODO: handling multiple animations in the same file (layers?)
+
+		for (const auto& c : connections_op_)
+		{
+			// Get bone's animation curve node and targeted property
+
+			const auto& [curve_node_id, model_id, property_name] = c;
+
+			const auto model_it = std::find_if(models_.begin(), models_.end(),
+					[&](const Model& model){ return model.id == model_id; });
+
+			if (model_it == models_.end())
+				continue;
+
+			const auto curve_node_it = std::find_if(animation_curve_nodes_.begin(), animation_curve_nodes_.end(),
+					[&](const AnimationCurveNode& node){ return node.id == curve_node_id; });
+
+			if (curve_node_it == animation_curve_nodes_.end())
+				continue;
+
+			for (const auto& c_ : connections_op_)
+			{
+				// Get animation curve node's animation curves and targeted property
+
+				const auto& [curve_id, curve_node_id_, property_name_] = c_;
+
+				if (curve_node_id != curve_node_id_)
+					continue;
+
+				const auto curve_it = std::find_if(animation_curves_.begin(), animation_curves_.end(),
+						[&](const AnimationCurve& curve){ return curve.id == curve_id; });
+
+				if (curve_it == animation_curves_.end())
+					continue;
+
+				// Compare node value with curve default (*)
+				set_missing_values(curve_node_it->properties, std::array<std::optional<double>, 1>{curve_it->default_value},
+						property_name_, false);
+
+				// Get animation
+
+				size_t anim_id = 6; // 6 indicates ignored
+
+				// Get transformation type offset
+				if (property_name == "Lcl Translation"s)
+					anim_id = 0;
+				else if (property_name == "Lcl Rotation"s)
+					anim_id = 3;
+
+				// Get axis offset
+				if (property_name_ == "d|Z"s)
+					anim_id += 2;
+				else if (property_name_ == "d|Y"s)
+					++anim_id;
+				else if (property_name_ != "d|X"s)
+					anim_id = 6; // not d|<axis>, ignore
+
+				if (anim_id != 6)
+					model_it->animation[anim_id] = &curve_it->animation;
+			}
+
+			// Compare model value with node's (*)
+			set_missing_values(model_it->properties, curve_node_it->properties.d(), property_name, true);
+
+			// (*) should be equal if both are present, if the former isn't, then try to get a value from the latter
+		}
+
+		using RT = geometry::RigidTransformation<geometry::Quaternion, Vec3>;
+		using KA_RT = geometry::KeyframedAnimation<std::vector, AnimTimeT, RT>;
+		using KA_DQ = geometry::KeyframedAnimation<std::vector, AnimTimeT, geometry::DualQuaternion>;
+
+		add_attribute<Vec3, Skeleton::Joint>(*skeleton, "position");
+		// TODO improve names (e.g. using file name) and handle collisions
+		// (expecting segfault on second import from returned nullptr due to name already being taken)
+		auto& attr_anim_rt = *add_attribute<KA_RT, Skeleton::Bone>(*skeleton, "TODO RT placeholder name");
+		auto& attr_anim_rt_b = *add_attribute<KA_RT, Skeleton::Bone>(*skeleton, "TODO RT binding placeholder name");
+		auto& attr_anim_dq = *add_attribute<KA_DQ, Skeleton::Bone>(*skeleton, "TODO DQ placeholder name");
+		auto& attr_anim_dq_b = *add_attribute<KA_DQ, Skeleton::Bone>(*skeleton, "TODO DQ binding placeholder name");
+
+		for (Model& m : models_)
+		{
+			if (m.type != ModelType::LimbNode)
+				continue;
+
+			KA_RT anim_rt; // RigidTransformation animation
+			KA_RT anim_rt_b; // RigidTransformation bind pose
+			KA_DQ anim_dq; // DualQuaternion animation
+			KA_DQ anim_dq_b; // DualQuaternion bind pose
+
+			const auto& bone = m.bone;
+			const auto& anims = m.animation;
+			auto& properties = m.properties;
+
+			// Get keyframe times
+			std::set<AnimTimeT> times;
+			{
+				std::vector<AnimationT> anim_copies;
+				for (const auto& anim : anims)
+					if (anim)
+						anim_copies.push_back(*anim);
+				times = AnimationT::get_unique_keyframe_times(anim_copies);
+			}
+
+			const auto& rotation_order = RotationOrder::XYZ;
+
+			const auto lcl_translation_value_d = [&](const size_t& index) {
+					return properties.lcl_translation()[index].value_or(0.0);
+			};
+
+			auto pre_rotation = from_euler(properties.pre_rotation());
+			auto post_rotation = from_euler(properties.post_rotation());
+			auto lcl_rotation = from_euler(properties.lcl_rotation());
+			auto total_lcl_rotation = pre_rotation * lcl_rotation * post_rotation;
+			Vec3 lcl_translation{lcl_translation_value_d(0), lcl_translation_value_d(1), lcl_translation_value_d(2)};
+
+			anim_rt_b.emplace_back(0.0, RT{total_lcl_rotation, lcl_translation});
+			anim_dq_b.emplace_back(0.0, geometry::DualQuaternion::from_tr(lcl_translation, total_lcl_rotation));
+
+			// We assume the interpolation to be linear, so we can create full transform keyframes by interpolating
+			// each component along its own animation, instead of passing the split components beyond
+			for (const auto& time : times)
+			{
+				const auto has_component = [&](const size_t& offset){
+						return anims[offset] || anims[offset + 1] || anims[offset + 2]; };
+				const auto anim_value = [&](const size_t& index) -> std::optional<AnimScalar> {
+						return anims[index] ? anims[index]->get_value(time) : std::optional<AnimScalar>(); };
+
+				auto t = has_component(0) ?
+						Vec3{anim_value(0).value_or(0.0), anim_value(1).value_or(0.0), anim_value(2).value_or(0.0)} :
+						lcl_translation;
+				auto r = has_component(3) ?
+						geometry::Quaternion{pre_rotation
+								* from_euler(std::array<std::optional<AnimScalar>, 3>
+										{anim_value(3), anim_value(4), anim_value(5)})
+								* post_rotation} :
+						total_lcl_rotation;
+
+				anim_rt.emplace_back(time, RT{r, t});
+				anim_dq.emplace_back(time, geometry::DualQuaternion::from_tr(t, r));
+			}
+
+			if (times.empty())
+			{
+				anim_rt = anim_rt_b;
+				anim_dq = anim_dq_b;
+			}
+
+			attr_anim_rt[bone] = anim_rt;
+			attr_anim_rt_b[bone] = anim_rt_b;
+			attr_anim_dq[bone] = anim_dq;
+			attr_anim_dq_b[bone] = anim_dq_b;
+		}
+
+		std::string name;
+		bool inserted = false;
+		while (std::tie(std::ignore, inserted) = skeletons.try_emplace(name, skeleton), !inserted)
+			name += '_'; // disambiguate however we can
 	}
 
 public:
@@ -264,6 +520,9 @@ public:
 		if (load_skeletons)
 			importer.load_skeletons(skeletons);
 	}
+
+private:
+	static inline const std::array<Vec3, 3> AXES = {Vec3{1, 0, 0}, Vec3{0, 1, 0}, Vec3{0, 0, 1}};
 };
 
 } // namespace io
